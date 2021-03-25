8e5a052ce0ba595df2b6a2d431c9286141035a43
Yunus Emre Topal <emretopal20@gmail.com>
2021-03-25 22:40:07 +0300

    Settings menu almost done
    Only needs a turkish volume image.

--- SoapRUSH/Assets/Scripts/Ui scripts/SettingsMenu.cs
+++ SoapRUSH/Assets/Scripts/Ui scripts/SettingsMenu.cs
@@ -4,58 +4,51 @@ using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
-    public Text volume;
-    public Text ses;
+    public Image volume;
 
-    public Text geri;
-    public Text retu;
-   public AudioMixer audioMixer;
-   public Slider slider;
-   public Dropdown dropdown;
-    void Start() {
+    public Image ses;
+    public AudioMixer audioMixer;
+    public Slider slider;
+    void Start()
+    {
         //initial volume settings
         float f = PlayerPrefs.GetFloat("Vol");
-        audioMixer.SetFloat("MainVolume",f);
+        audioMixer.SetFloat("MainVolume", f);
         slider.value = f;
 
         // initial language settings
         int lang = PlayerPrefs.GetInt("Lang");
-        dropdown.value = lang;
-        if(lang == 0){
-            volume.enabled = false;
-            ses.enabled = true;
-            retu.enabled = false;
-            geri.enabled = true;
+        if (lang == 0)
+        {
+            LangtoTurkish();
         }
-        else{
-            retu.enabled = true;
-            geri.enabled = false;
-            volume.enabled = true;
-            ses.enabled = false;
+        else
+        {
+            LangtoEnglish();
         }
+
     }
 
-   public void SetVolume(float volume){
-       audioMixer.SetFloat("MainVolume", volume);
-       PlayerPrefs.SetFloat("Vol",volume);
-   }
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("MainVolume", volume);
+        PlayerPrefs.SetFloat("Vol", volume);
+    }
 
-//change texts according to the language choice and save it for the other scenes.
-   public void HandleInputData(int val){
-       int value = dropdown.value;
-       if(value == 0){
-           PlayerPrefs.SetInt("Lang",0);
-           volume.enabled = false;
-           ses.enabled = true;
-           retu.enabled = false;
-           geri.enabled = true;
-       }
-       else{
-           PlayerPrefs.SetInt("Lang",1);
-           retu.enabled = true;
-           geri.enabled = false;
-           volume.enabled = true;
-           ses.enabled = false;
-       }
-   }
+
+    public void LangtoTurkish()
+    {
+        PlayerPrefs.SetInt("Lang", 0);
+        volume.enabled = false;
+        ses.enabled = true;
+
+    }
+    public void LangtoEnglish()
+    {
+        PlayerPrefs.SetInt("Lang", 1);
+        volume.enabled = true;
+        ses.enabled = false;
+    }
+
+
 }
