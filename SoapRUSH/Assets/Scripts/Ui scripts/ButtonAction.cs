2fc6d8514ec95834950a426785b20b9d1b0f2b1b
Yunus Emre Topal <emretopal20@gmail.com>
2021-04-06 23:57:22 +0300

    Pause menu completed
    Previous commit did not have any button actions. This one has everything.

--- SoapRUSH/Assets/Scripts/Ui scripts/ButtonAction.cs
+++ SoapRUSH/Assets/Scripts/Ui scripts/ButtonAction.cs
@@ -1,27 +1,40 @@
-﻿using UnityEngine;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ButtonAction : MonoBehaviour
 {
+
     // Start is called before the first frame update
-    
-    public void GameScene(){
-        SceneManager.LoadScene("Game");
+
+    public void GameScene()
+    {
+        SceneManager.LoadScene("Level1");
     }
-    public void SettingsMenu(){
+    public void SettingsMenu()
+    {
         SceneManager.LoadScene("Settings");
     }
-    public void MainMenu(){
+    public void MainMenu()
+    {
         SceneManager.LoadScene("MainMenu");
     }
-    public void PauseMenu(){
-        SceneManager.LoadScene("PauseMenu");
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0;
     }
-    public void LevelManu(){
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1;
+    }
+    public void LevelManu()
+    {
         SceneManager.LoadScene("Level");
     }
-    public void AboutMenu(){
+    public void AboutMenu()
+    {
         SceneManager.LoadScene("AboutMenu");
     }
-    
+
 }
