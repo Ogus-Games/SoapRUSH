3fff3348a07f102761f80e7958cc94ff6ec57384
MelihCelik00 <melihsafa.c@gmail.com>
2021-05-01 21:50:22 +0300

    ad manager update

---
+++ SoapRUSH/Assets/Scripts/Managers/AdManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using GoogleMobileAds.Api;
+
+namespace Managers
+{
+    public class AdManager : MonoBehaviour
+    {
+        public string appID = "ca-app-pub-6734054441405950~2171722172";
+        public string interstitialID = "ca-app-pub-6734054441405950/5526829048";
+        
+    }
+}
